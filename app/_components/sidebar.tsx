eb98a32a3e3ee35fc54491a131b62f5014aebd88
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-03 22:08:26 -0300

    :sparkles: feat: add initial Sidebar component

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const sidebar = () => {
-  return <div>sidebar</div>;
+const Sidebar = () => {
+  return (
+    <aside className="w-64 h-screen bg-white">
+      <div>
+        <h1 className="text-[#00a180] text-2xl font-extrabold px-8 py-6">
+          STOCKLY
+        </h1>
+      </div>
+      <div className="flex flex-col gap-2 p-2.5">
+        <button className="text-[#00a180] bg-[#00a180]/10 py-3 px-6 text-left rounded-lg">
+          Dashboard
+        </button>
+        <button className="text-[#00a180] bg-[#00a180]/10 py-3 px-6 text-left rounded-lg">
+          Produtos
+        </button>
+      </div>
+    </aside>
+  );
 };
 
-export default sidebar;
+export default Sidebar;
