0e993f40c862bd692cf8fa20a06ebf751e50b8ad
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-06 15:56:56 -0300

    ✨ feat: add shadcn DataTable component

---
+++ app/products/_components/table-columns.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor unitário",
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(amount);
+      return <div className="">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "",
+    header: "Ações",
+  },
+];
