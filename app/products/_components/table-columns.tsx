01745ec1e693420e553ce5f2a690278c2ef4f3ea
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-06 16:32:17 -0300

    ✨ feat: add shadcn Badge to Products table

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Badge } from "@/app/_components/ui/badge";
 import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import { Circle } from "lucide-react";
 
 export const productTableColumns: ColumnDef<Product>[] = [
   {
@@ -12,11 +14,11 @@ export const productTableColumns: ColumnDef<Product>[] = [
     accessorKey: "price",
     header: "Valor unitário",
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("price"));
+      const price = parseFloat(row.getValue("price"));
       const formatted = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
-      }).format(amount);
+      }).format(price);
       return <div className="">{formatted}</div>;
     },
   },
@@ -27,6 +29,22 @@ export const productTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+      const statusBadge =
+        status === "IN_STOCK" ? (
+          <Badge className="gap-1.5 bg-[#00A180]/10 text-[#00A180] hover:bg-[#00A180]/15">
+            <Circle size={10} className="fill-current" />
+            Em estoque
+          </Badge>
+        ) : (
+          <Badge className="gap-1.5 bg-slate-500/10 text-slate-500 hover:bg-slate-500/15">
+            <Circle size={10} className="fill-current" />
+            Esgotado
+          </Badge>
+        );
+      return statusBadge;
+    },
   },
   {
     accessorKey: "",
