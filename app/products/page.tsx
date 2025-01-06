82ce65535adf023a4cc92660ae562009b5ba8746
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-06 15:57:41 -0300

    💄 feat: add initial styles to Products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumns } from "./_components/table-columns";
 
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+const ProductsPage = async () => {
+  const products = await db.product.findMany();
+
+  return (
+    <section className="w-[1188px] space-y-5 p-8">
+      <div className="flex w-full justify-between">
+        <div className="space-y-2">
+          <span className="text-xs font-semibold text-[#00A180]">Produtos</span>
+          <h1 className="text-xl font-semibold text-slate-900">
+            Gestão de produtos
+          </h1>
+        </div>
+        <Button className="self-end">
+          <PlusIcon />
+          Novo produto
+        </Button>
+      </div>
+      <DataTable columns={productTableColumns} data={products} />
+    </section>
+  );
 };
 
 export default ProductsPage;
