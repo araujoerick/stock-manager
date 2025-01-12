dea75d7542b80b137e8f49de053ae74f59355a0c
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-12 01:13:42 -0300

    ✨ feat: add form to AddProductButton

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,7 @@
-import { PlusIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
 import { getProducts } from "../_data-access/product/get-products";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../_components/ui/dialog";
+import AddProductButton from "./_components/add-product-buttons";
 
 const ProductsPage = async () => {
   const products = await getProducts();
@@ -24,22 +15,7 @@ const ProductsPage = async () => {
             Gestão de produtos
           </h1>
         </div>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button className="self-end">
-              <PlusIcon />
-              Novo produto
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Adicionar novo produto</DialogTitle>
-              <DialogDescription>
-                Insira as informações do produto.
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+        <AddProductButton />
       </div>
       <DataTable
         columns={productTableColumns}
