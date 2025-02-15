b6622409786c9f2c2b5c48ae8a59cf39193c54aa
Erick Araújo <araujoerick.dev@gmail.com>
2025-02-14 23:01:43 -0300

    ✨ feat: add initial form

--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -1,12 +1,45 @@
+"use client";
+
+import { Combobox, ComboboxOption } from "@/app/_components/ui/combobox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/_components/ui/form";
+import { Input } from "@/app/_components/ui/input";
 import {
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
 } from "@/app/_components/ui/sheet";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-const UpsertSheetContent = () => {
+const formSchema = z.object({
+  productId: z.string().uuid(),
+  quantity: z.number().int().positive(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface UpsertSheetContentProps {
+  productOptions: ComboboxOption[];
+}
+
+const UpsertSheetContent = ({ productOptions }: UpsertSheetContentProps) => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      productId: "",
+      quantity: 1,
+    },
+  });
+
   return (
     <SheetContent>
       <SheetHeader>
@@ -15,6 +48,46 @@ const UpsertSheetContent = () => {
           Preencha os campos abaixo para adicionar uma nova venda.
         </SheetDescription>
       </SheetHeader>
+
+      <Form {...form}>
+        <form className="space-y-6 py-6">
+          <FormField
+            control={form.control}
+            name="productId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Produto</FormLabel>
+                <FormControl>
+                  <Combobox
+                    placeholder="Selecione um produto"
+                    {...field}
+                    options={productOptions}
+                  />
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="quantity"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Quantidade</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Digite a quantidade"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </SheetContent>
   );
 };
