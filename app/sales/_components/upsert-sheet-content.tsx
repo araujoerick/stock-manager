89f14910b035a6de1e89cc537f608a72fb0127ca
Erick Araújo <araujoerick.dev@gmail.com>
2025-03-05 12:25:17 -0300

    ✨ feat: porduct stock is validated before adding product to sale

--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -84,6 +84,17 @@ const UpsertSheetContent = ({
         (product) => product.id === selectedProduct.id,
       );
       if (existingProduct) {
+        const productIsOutOfStock =
+          existingProduct.quantity + data.quantity > selectedProduct.stock;
+        if (productIsOutOfStock) {
+          form.setError("quantity", {
+            message: "Quantidade indisponível em estoque",
+          });
+          return currentProducts;
+        }
+
+        form.reset();
+
         return currentProducts.map((product) => {
           if (product.id === selectedProduct.id) {
             return {
@@ -94,6 +105,17 @@ const UpsertSheetContent = ({
           return product;
         });
       }
+
+      const productIsOutOfStock = data.quantity > selectedProduct.stock;
+      if (productIsOutOfStock) {
+        form.setError("quantity", {
+          message: "Quantidade indisponível em estoque",
+        });
+        return currentProducts;
+      }
+
+      form.reset();
+
       return [
         ...currentProducts,
         {
@@ -103,8 +125,6 @@ const UpsertSheetContent = ({
         },
       ];
     });
-
-    form.reset();
   };
 
   const productsTotal = useMemo(() => {
