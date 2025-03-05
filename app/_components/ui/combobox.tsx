86d6f149725523bc310e0f960021a51ca79fad63
Erick Araújo <araujoerick.dev@gmail.com>
2025-03-05 12:25:41 -0300

    💄 feat: change combobox max width

--- app/_components/ui/combobox.tsx
+++ app/_components/ui/combobox.tsx
@@ -56,7 +56,7 @@ export const Combobox = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="max-w-[300px] p-0">
         <Command>
           <CommandInput
             placeholder="Procurar produtos..."
