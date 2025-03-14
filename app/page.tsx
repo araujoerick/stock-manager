e7eceb24cbf632ec2e53e51061d23509803fa77c
Erick Araújo <araujoerick.dev@gmail.com>
2025-03-14 17:46:44 -0300

    ✨ feat: add initial dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main className="p-8">Home teste</main>;
+  return (
+    <main className="w-[1188px] space-y-5 p-8">
+      <div className="flex w-full justify-between">
+        <div className="space-y-2">
+          <span className="text-xs font-semibold text-primary">Dashboard</span>
+          <h1 className="text-xl font-semibold text-slate-900">Dashboard</h1>
+        </div>
+      </div>
+    </main>
+  );
 }
