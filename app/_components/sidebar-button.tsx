92f06e6eecf4aab06597fec330e6f43e83a93935
Erick Araújo <araujoerick.dev@gmail.com>
2025-04-09 12:53:08 -0300

    📱 turn sidebar responsive

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -7,18 +7,25 @@ import Link from "next/link";
 interface SidebarButtonProps {
   children: React.ReactNode;
   href: string;
+  icon: React.ReactNode;
 }
 
-const SidebarButton = ({ children, href }: SidebarButtonProps) => {
+const SidebarButton = ({ children, href, icon }: SidebarButtonProps) => {
   const pathname = usePathname();
 
   return (
     <Button
-      variant={pathname === href ? "secondary" : "ghost"}
+      variant={pathname === href ? "default" : "ghost"}
       className="justify-start"
       asChild
     >
-      <Link href={href}>{children}</Link>
+      <Link
+        href={href}
+        className="grid w-full items-center justify-center gap-2 p-2 text-sm font-medium text-primary md:grid-cols-[auto,1fr]"
+      >
+        {icon}
+        <p className="hidden md:inline-block">{children}</p>
+      </Link>
     </Button>
   );
 };
--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -3,23 +3,23 @@ import SidebarButton from "./sidebar-button";
 
 const Sidebar = () => {
   return (
-    <aside className="h-screen w-64 bg-white">
+    <aside className="min-h-screen w-64 bg-white">
       <div>
-        <h1 className="px-8 py-6 text-2xl font-extrabold text-primary">
-          STOCKLY
+        <h1 className="hidden px-8 py-6 text-2xl font-extrabold text-primary md:inline-block">
+          STOCK MANAGER
+        </h1>
+        <h1 className="px-8 py-6 text-center text-2xl font-extrabold text-primary md:hidden">
+          SM
         </h1>
       </div>
       <div className="flex flex-col gap-2 p-2.5">
-        <SidebarButton href="/">
-          <LayoutGrid size={20} />
+        <SidebarButton href="/" icon={<LayoutGrid size={20} />}>
           Dashboard
         </SidebarButton>
-        <SidebarButton href="/products">
-          <Package size={20} />
+        <SidebarButton href="/products" icon={<Package size={20} />}>
           Produtos
         </SidebarButton>
-        <SidebarButton href="/sales">
-          <ShoppingBasket size={20} />
+        <SidebarButton href="/sales" icon={<ShoppingBasket size={20} />}>
           Vendas
         </SidebarButton>
       </div>
